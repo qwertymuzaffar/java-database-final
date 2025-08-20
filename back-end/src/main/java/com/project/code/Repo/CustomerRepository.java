9f229f577d3311f3c19a5c1c9d6738453d86953e
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-08-20 15:46:11 +0500

    Optional for single-result lookups

--- back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
+++ back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
@@ -20,7 +21,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     //      - This method will allow you to find a customer by their email address.
     //      - Return type: Customer
     //      - Parameter: String email
-    public Customer findByEmail(String email);
+    public Optional<Customer> findByEmail(String email);
       
     // Example: public Customer findByEmail(String email);
 
